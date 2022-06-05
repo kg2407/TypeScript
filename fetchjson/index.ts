64c9ba17383600c87bff115145c3e2d60e81ce76
kg2407 <iamkg2407@gmail.com>
2022-06-05 09:54:43 +0530

    TypeScript Basics

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,17 +1,28 @@
 import axios from 'axios'
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-  const todo = response.data
+  const todo = response.data as Todo
 
   const ID = todo.id
   const title = todo.title
   const finished = todo.completed
+  logTodo(ID, title, finished)
+  
+})
 
+const logTodo = (id: number, title: string, finished: boolean) => {
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has title of: ${title}
     Is it finished? ${finished} 
   `)
-})
+  
+}
